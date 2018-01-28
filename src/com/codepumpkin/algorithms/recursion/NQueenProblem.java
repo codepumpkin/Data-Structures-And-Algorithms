5822e79107fa148b4e5ef8781cd4e2e838837283
CodePumpkin <admin@codepumpkin.com>
2018-01-28 19:24:46 +0530

    N Queen Problem

--- src/com/codepumpkin/algorithms/recursion/NQueenProblem.java
+++ src/com/codepumpkin/algorithms/recursion/NQueenProblem.java
@@ -11,6 +11,7 @@ public class NQueenProblem {
 
 	public static void main(String[] args) {
 
+		System.out.println("Enter Number of Queens");
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
 
